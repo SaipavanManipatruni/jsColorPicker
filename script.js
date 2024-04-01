6e7dd835f9055ac86c3e7df6dc9f4bdaba5aaebf
SaipavanManipatruni <saipavan1011@gmail.com>
2024-04-01 19:21:48 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,22 @@
+var bodyElement = document.getElementById("body");
+
+function buttonOne() {
+  bodyElement.style.backgroundColor = "#e0e0e0";
+  selectedColorHexCode.textContent = "#e0e0e0";
+  selectedColorHexCode.style.color = "#e0e0e0";
+}
+function buttonTwo() {
+  bodyElement.style.backgroundColor = "#6fcf97";
+  selectedColorHexCode.textContent = "#6fcf97";
+  selectedColorHexCode.style.color = "#6fcf97";
+}
+function buttonThree() {
+  bodyElement.style.backgroundColor = "#56ccf2";
+  selectedColorHexCode.textContent = "#56ccf2";
+  selectedColorHexCode.style.color = "#56ccf2";
+}
+function buttonFour() {
+  bodyElement.style.backgroundColor = "#bb6bd9";
+  selectedColorHexCode.textContent = "#bb6bd9";
+  selectedColorHexCode.style.color = "#bb6bd9";
+}
